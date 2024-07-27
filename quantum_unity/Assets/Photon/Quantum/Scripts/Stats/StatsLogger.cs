e497235d4478943894752b1ece1bbf9b7583166b
Diar03 <d.kamberi@student.vu.nl>
2024-07-28 00:55:21 +0200

    [TEST] 20 tick - 50ms Latency

--- quantum_unity/Assets/Photon/Quantum/Scripts/Stats/StatsLogger.cs
+++ quantum_unity/Assets/Photon/Quantum/Scripts/Stats/StatsLogger.cs
@@ -17,7 +17,7 @@ public class QuantumLogger : MonoBehaviour
 
     void Start()
     {
-        logFilePath = $"latency_output/output_300ms/stats_log_{nProcessID}.csv"; // Unique file per process
+        logFilePath = $"latency_output/output_20tick_100ms/stats_log_{nProcessID}.csv"; // Unique file per process
 
         // create event system if none exists in the scene
         var eventSystems = FindObjectsOfType<EventSystem>();

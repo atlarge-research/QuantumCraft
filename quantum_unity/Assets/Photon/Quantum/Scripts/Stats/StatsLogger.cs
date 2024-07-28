650eec60cc0fb1190ec29dd43c53e0189b5259c3
Diar03 <d.kamberi@student.vu.nl>
2024-07-28 02:04:43 +0200

    [TEST] 20 tick - 200ms Latency

--- quantum_unity/Assets/Photon/Quantum/Scripts/Stats/StatsLogger.cs
+++ quantum_unity/Assets/Photon/Quantum/Scripts/Stats/StatsLogger.cs
@@ -17,7 +17,7 @@ public class QuantumLogger : MonoBehaviour
 
     void Start()
     {
-        logFilePath = $"latency_output/output_20tick_150ms/stats_log_{nProcessID}.csv"; // Unique file per process
+        logFilePath = $"latency_output/output_20tick_200ms/stats_log_{nProcessID}.csv"; // Unique file per process
 
         // create event system if none exists in the scene
         var eventSystems = FindObjectsOfType<EventSystem>();
--- quantum_unity/Assets/Photon/QuantumDemo/Menu/Scripts/UIConnect.cs
+++ quantum_unity/Assets/Photon/QuantumDemo/Menu/Scripts/UIConnect.cs
@@ -87,7 +87,7 @@ namespace Quantum.Demo
             Debug.Log($"Using user name '{Username.text}'");
             CustomLogger.Instance.Log($"Using user name '{Username.text}'");
 
-            SimLatency = 75;
+            SimLatency = 100;
             
             UIMain.Client = new QuantumLoadBalancingClient(PhotonServerSettings.Instance.AppSettings.Protocol);
 

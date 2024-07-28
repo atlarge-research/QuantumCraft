15314d388d8f2280513a417655dcfc5a412104bd
Diar03 <d.kamberi@student.vu.nl>
2024-07-28 23:24:39 +0200

    [TEST] Rollback Latency 150ms

--- quantum_unity/Assets/Photon/QuantumDemo/Menu/Scripts/UIConnect.cs
+++ quantum_unity/Assets/Photon/QuantumDemo/Menu/Scripts/UIConnect.cs
@@ -87,7 +87,7 @@ namespace Quantum.Demo
             Debug.Log($"Using user name '{Username.text}'");
             CustomLogger.Instance.Log($"Using user name '{Username.text}'");
 
-            SimLatency = 150;
+            SimLatency = 75;
             
             UIMain.Client = new QuantumLoadBalancingClient(PhotonServerSettings.Instance.AppSettings.Protocol);
 

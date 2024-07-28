fc89f631ea860c6877855825f3032b214370c7c7
Diar03 <d.kamberi@student.vu.nl>
2024-07-28 22:57:09 +0200

    [TEST] Rollback Latency 200ms

--- quantum_unity/Assets/Scripts/LoggerScript.cs
+++ quantum_unity/Assets/Scripts/LoggerScript.cs
@@ -43,7 +43,7 @@ public class CustomLogger : MonoBehaviour
 
             // Get the current process ID and format the file name
             int processId = Process.GetCurrentProcess().Id;
-            logFilePath = $"latency_output/output_100ms/log_{processId}.txt";
+            logFilePath = $"latency_output/output_200ms/log_{processId}.txt";
 
             // Create the StreamWriter to write to the log file
             writer = new StreamWriter(logFilePath, true); // Append to the file if it exists
